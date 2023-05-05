6f8fd7387b857d8e72f59b87ddca323e75c09dea
Egor <itpd2010@yandex.ru>
2023-05-06 01:23:38 +0300

    feat: finish 01-read-file task

---
+++ 01-read-file/index.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+const { stdout } = process;
+
+const stream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
+
+let data = '';
+
+stream.on('data', chunk => data += chunk);
+stream.on('end', () => stdout.write(data));
+stream.on('error', error => console.log('Error', error.message));
